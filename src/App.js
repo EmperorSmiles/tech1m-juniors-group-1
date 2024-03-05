75c8d921a07b83436b5c601a6182e0487262d588
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-03-05 17:25:47 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Course from "./pages/Course";
+import Messages from "./pages/Messages";
+import Payment from "./pages/Payment";
+import Quiz from "./pages/Quiz";
+import Students from "./pages/Students";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+import SignIn from "./pages/SignIn";
+import {CoursesData, StudentsData, Paydata, AssignmentData} from './data'
+import ForgotPassword from "./pages/ForgotPassword";
+import MainLayout from "./components/MainLayout";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      <div className="App">
+            <Router>
+              <Routes >
+                <Route path='/in/' element={<MainLayout/>}>
+                  <Route path="home" element={<Home />} />
+                  <Route path="courses" element={<Course cdata = {CoursesData}/>} />
+                  <Route path="students" element={<Students sdata = {StudentsData}/>} />
+                  <Route path="payments" element={<Payment pdata = {Paydata}/>} />
+                  <Route path="quiz" element={<Quiz bdata = {AssignmentData}/>} />
+                  <Route path="messages" element={<Messages />} />
+                </Route>
+                <Route path='/login' element={<Login/>} />
+                <Route path='/' element={<Welcome/>} />
+                <Route path='/signup' element={<SignIn/>} />
+                <Route path='/forgotPassword' element={<ForgotPassword/>} />
+              </Routes>
+            </Router>
+      
+      
+      </div>
+  )
 }
 
 export default App;
