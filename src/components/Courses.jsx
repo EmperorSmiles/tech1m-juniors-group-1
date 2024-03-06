b67820bb68e58021ff3c0bd6d886a2fe5eac29e8
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-03-06 12:54:59 +0100

    Updated

---
+++ src/components/Courses.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SingleCourse from "./SingleCourse";
+
+const Courses = ({ course }) => {
+    return (
+        <div>
+            <div className="my-5 px-5 md:px-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid-rows-2 gap-6 ">
+                {course.map((course) => {
+                    return <SingleCourse course={course} key={course.id} />;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Courses;
